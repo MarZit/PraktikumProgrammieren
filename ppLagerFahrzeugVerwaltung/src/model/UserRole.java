c1451d8778f922c6e4240c6f831f5e6d45051e18
Schule <Schule@User-TOSH>
2017-11-23 12:29:34 +0100

    Queries neu Entities + SQL-Befehl zur Datenbankerzeugung neu

---
+++ ppLagerFahrzeugVerwaltung/src/model/UserRole.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the user_role database table.
+ * 
+ */
+@Entity
+@Table(name="user_role")
+@NamedQuery(name="UserRole.findAll", query="SELECT u FROM UserRole u")
+public class UserRole implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="role_name")
+	private String roleName;
+
+	private String username;
+
+	public UserRole() {
+	}
+
+	public String getRoleName() {
+		return this.roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
